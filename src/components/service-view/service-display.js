9245dff71ed778c95645052caaf77840dc006ab5
Kushagra <kushagra.acharya@outlook.com>
2024-05-04 22:53:41 +0545

    feat: service section added

--- src/components/service-view/service-display.js
+++ src/components/service-view/service-display.js
@@ -4,11 +4,11 @@ import ServiceItemView from "./service-item";
 const ServiceDisplayView = () => {
   return (
     <>
-      <div className="bg-white">
+      <div className="bg-slate-50 pb-5">
         <div className="flex justify-center items-center text-center py-10">
-          <p className="text-3xl">
-            Explore our <span className="">Best-Selling</span> <br /> treatments
-            available
+          <p className="text-4xl">
+            Explore our <span className="text-blue-700">Best-Selling</span>{" "}
+            <br /> treatments available
           </p>
         </div>
         <div className="max-w-screen-2xl mx-auto px-4">
@@ -37,6 +37,9 @@ const ServiceDisplayView = () => {
               />
             </div>
           </div>
+        </div>
+        <div className=" flex border-green-500 border-2 text-green-600 w-fit justify-center items-center mx-auto my-5 rounded-lg transition duration-200 ease-in-out hover:bg-green-700 hover:text-white hover:border-green-700">
+          <div className="text-center px-5 py-2">See All Services</div>
         </div>
       </div>
     </>
--- src/components/top-navbar.js
+++ src/components/top-navbar.js
@@ -1,39 +1,52 @@
 import React from "react";
+import { UsersIcon, HomeIcon } from "@heroicons/react/outline";
 
 const NavbarTop = () => {
   return (
-    <nav className="bg-gray-300 flex items-center justify-between mx-100 ">
+    <nav className=" flex items-center justify-between mx-100 py-3 shadow-md  sticky top-0 bg-white z-50">
       <div>
-        <div className="w-10 h-10 bg-red-400 flex justify-center items-center">
-          X
+        <div className="w-10 h-10 flex justify-center items-center">
+          <HomeIcon className="w-7 h-7 text-blue-500 ml-3" />
         </div>
       </div>
       <ul className="flex items-center justify-center">
         <li className="ml-6">
-          <a href="#" className="hover:text-gray-300">
+          <a
+            href="#"
+            className="transition duration-200 ease-in-out hover:text-blue-600 "
+          >
             Home
           </a>
         </li>
         <li className="ml-6">
-          <a href="#" className="hover:text-gray-300">
+          <a
+            href="#"
+            className="transition duration-200 ease-in-out hover:text-blue-600 "
+          >
             About
           </a>
         </li>
         <li className="ml-6">
-          <a href="#" className="hover:text-gray-300">
+          <a
+            href="#"
+            className="transition duration-200 ease-in-out hover:text-blue-600 "
+          >
             Services
           </a>
         </li>
         <li className="ml-6">
-          <a href="#" className="hover:text-gray-300">
+          <a
+            href="#"
+            className="transition duration-200 ease-in-out hover:text-blue-600 "
+          >
             Contact
           </a>
         </li>
       </ul>
       <div className="flex items-center">
-        <a href="#" className="mr-4 hover:text-gray-300">
-          Book Now
-        </a>
+        <button className="px-4 py-2 bg-transparent-500 text-blue-600 rounded-full text-sm font-semibold">
+          LOGIN
+        </button>
       </div>
     </nav>
   );
